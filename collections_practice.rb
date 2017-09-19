cee4d70c77e9c1729d6c63b4ef5ff13c84136ba3
Justin Daniel <justinddaniel@gmail.com>
2017-09-19 20:32:11 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,3 +2,19 @@ def sort_array_asc(array)
   array.sort
 end
 
+def sort_array_desc(array)
+  array.sort
+  array.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.chars.length == b.chars.length
+      0
+    elsif a.chars.length < b.chars.length
+      -1
+    elsif a.chars.length > b.chars.length
+      1
+    end
+  end
+  
